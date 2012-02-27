41bd6cae04999dd91168b29c558a7ba0d19fcdd7
James Reeves <jreeves@weavejester.com>
2012-02-27 20:49:26 +0000

    Functions to read objects from PEM files

--- src/crypto/keystore.clj
+++ src/crypto/keystore.clj
@@ -1,21 +1,42 @@
 (ns crypto.keystore
   "Functions for creating and managing Java Keystores."
   (:require [clojure.java.io :as io])
-  (:import java.security.KeyStore
+  (:import java.io.StringReader
+           java.security.KeyStore
+           java.security.KeyStore$PrivateKeyEntry
+           java.security.KeyPair
+           java.security.PrivateKey
            java.security.Security
+           java.security.cert.Certificate
            org.bouncycastle.openssl.PEMReader
            org.bouncycastle.openssl.PasswordFinder
            org.bouncycastle.jce.provider.BouncyCastleProvider))
 
 (Security/addProvider (BouncyCastleProvider.))
 
-(deftype Password [password]
+(deftype PasswordFn [password-fn]
   PasswordFinder
   (getPassword [_]
-    (.toCharArray (str password))))
+    (.toCharArray (password-fn))))
 
-(defn pem-reader [file password]
-  (PEMReader. (io/reader file) (Password. password)))
+(defprotocol PemSource
+  (pem-reader [source pass-fn]
+    "Create a PEMReader from a PEM source and optional password function."))
+
+(extend-protocol PemSource
+  String
+  (pem-reader [s pass-fn] (pem-reader (StringReader. s) pass-fn))
+  java.io.Reader
+  (pem-reader [r pass-fn] (PEMReader. r (PasswordFn. pass-fn)))
+  Object
+  (pem-reader [x pass-fn] (pem-reader (io/reader x) pass-fn)))
+
+(defn pem-seq
+  "Return a lazy seq of objects from a PEMReader."
+  [reader]
+  (take-while
+   (complement nil?)
+   (repeatedly #(.readObject reader))))
 
 (defn keystore
   "Create a blank KeyStore."
