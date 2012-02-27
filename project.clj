cd9ce9315d66e5b21d486e64b38ab889e655abde
James Reeves <jreeves@weavejester.com>
2012-02-27 00:57:57 +0000

    Included PEMReader from BouncyCastle

--- project.clj
+++ project.clj
@@ -1,3 +1,4 @@
 (defproject crypto-keystore "0.1.0-SNAPSHOT"
   :description "Library for managing Java keystores"
-  :dependencies [[org.clojure/clojure "1.2.1"]])
+  :dependencies [[org.clojure/clojure "1.2.1"]
+                 [org.bouncycastle/bcprov-jdk15 "1.46"]])
--- src/crypto/keystore.clj
+++ src/crypto/keystore.clj
@@ -1,6 +1,21 @@
 (ns crypto.keystore
   "Functions for creating and managing Java Keystores."
-  (:import java.security.KeyStore))
+  (:require [clojure.java.io :as io])
+  (:import java.security.KeyStore
+           java.security.Security
+           org.bouncycastle.openssl.PEMReader
+           org.bouncycastle.openssl.PasswordFinder
+           org.bouncycastle.jce.provider.BouncyCastleProvider))
+
+(Security/addProvider (BouncyCastleProvider.))
+
+(deftype Password [password]
+  PasswordFinder
+  (getPassword [_]
+    (.toCharArray (str password))))
+
+(defn pem-reader [file password]
+  (PEMReader. (io/reader file) (Password. password)))
 
 (defn keystore
   "Create a blank KeyStore."
