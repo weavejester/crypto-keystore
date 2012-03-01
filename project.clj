040604c003f2e0612606ba71d112e5536aba09cf
James Reeves <jreeves@weavejester.com>
2012-03-01 19:24:02 +0000

    Released 0.1.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject crypto-keystore "0.1.0-SNAPSHOT"
+(defproject crypto-keystore "0.1.0"
   :description "Library for managing Java keystores"
   :dependencies [[org.clojure/clojure "1.2.1"]
                  [org.bouncycastle/bcprov-jdk15 "1.46"]])
